26f0179c68d3cbd4bb753351e946054532e7b311
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2021-11-13 15:56:29 +0100

    add test command

---
+++ commands/test.js
@@ -0,0 +1,9 @@
+import { Utils } from '../utils.js';
+export const command = {
+    name: 'test',
+    description: 'Permet de vérifier si le bot est fonctionnel',
+    execute: function (client, integration) {
+        Utils.log(this.name, integration);
+        integration.reply(`Test OK ${client.emojis.resolve("681518586493272088").toString()}`);
+    }
+};
