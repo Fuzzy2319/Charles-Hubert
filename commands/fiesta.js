f772cb429f6d81a616ae47043ad28d2cdc47a84c
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2021-11-16 14:45:23 +0100

    change tsconfig

--- commands/fiesta.js
+++
@@ -1,9 +0,0 @@
-import { Utils } from '../utils.js';
-export const command = {
-    name: 'fiesta',
-    description: 'Envoie l\'emoji fiesta',
-    execute: function (client, interaction) {
-        Utils.log(this.name, interaction);
-        interaction.reply(client.emojis.resolve("756525370433601587").toString() + client.emojis.resolve("756525370433601587").toString() + client.emojis.resolve("756525370433601587").toString());
-    }
-};
--- commands/test.js
+++
@@ -1,9 +0,0 @@
-import { Utils } from '../utils.js';
-export const command = {
-    name: 'test',
-    description: 'Permet de vérifier si le bot est fonctionnel',
-    execute: function (client, interaction) {
-        Utils.log(this.name, interaction);
-        interaction.reply(`Test OK ${client.emojis.resolve("681518586493272088").toString()}`);
-    }
-};
