dbffbaf8a40446829441cd04d3c82fb5787169e7
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2021-11-22 14:05:44 +0100

    Add invite command

---
+++ commands/invite.ts
@@ -0,0 +1,20 @@
+import { Client, Collection, CommandInteraction, Invite } from 'discord.js'
+import { Utils } from '../utils.js'
+
+export const command = {
+    name: 'invite',
+    description: 'Envoie une invitation pour le serveur',
+    type: 1,
+    options: [],
+    execute: function (client: Client, interaction: CommandInteraction) {
+        Utils.log(this.name, interaction)
+
+        interaction.guild.invites.fetch().then((invites: Collection<string,Invite>) => {
+            if (invites.size === 0) {
+                interaction.reply(`Il n'y a pas d'invitation pour ${interaction.guild.name}`)
+            } else {
+                interaction.reply(`Utilise cette invitation pour inviter de nouveaux membres \n${invites.first().url}`)
+            }
+        })
+    }
+}
