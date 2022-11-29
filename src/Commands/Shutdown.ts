6c3e41905939c8b1ed5be88a911bf910feec0952
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2022-11-29 12:02:25 +0100

    Update dependencies and fix build errors

--- src/Commands/Shutdown.ts
+++ src/Commands/Shutdown.ts
@@ -2,6 +2,7 @@ import {Client, CommandInteraction, Locale, Message} from 'discord.js'
 import {AppSlashCommandBuilder} from '../Utils/Builder.js'
 import log from '../Utils/Logger.js'
 import sleep from '../Utils/Sleep.js'
+import {DiscordGatewayAdapterLibraryMethods} from "@discordjs/voice";
 
 const command: AppSlashCommandBuilder = (new AppSlashCommandBuilder())
     .setName('shutdown')
@@ -20,7 +21,7 @@ const command: AppSlashCommandBuilder = (new AppSlashCommandBuilder())
         await sleep(500)
         await message.edit('Arrêt de Charles-Hubert.')
         await sleep(500)
-        client.voice.adapters.forEach((adapter) => adapter.destroy())
+        client.voice.adapters.forEach((adapter: DiscordGatewayAdapterLibraryMethods) => adapter.destroy())
         await message.edit('Arrêt de Charles-Hubert..')
         await sleep(500)
         await message.edit('Arrêt de Charles-Hubert...')
