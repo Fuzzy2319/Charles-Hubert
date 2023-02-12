3d2cbc68ba01029fad4ce4fb3d1c17e0f40c849d
Fuzzy2319 <alexandre.verbrigghe@orange.fr>
2023-02-12 12:54:11 +0100

    Translate invite command

--- src/Commands/Invite.ts
+++ src/Commands/Invite.ts
@@ -1,23 +1,22 @@
-import {Client, Collection, CommandInteraction, Invite, Locale} from 'discord.js'
+import {Client, Collection, CommandInteraction, Invite} from 'discord.js'
 import {AppSlashCommandBuilder} from '../Utils/Builder.js'
+import translator from '../Utils/Translator.js'
 
 const command: AppSlashCommandBuilder = (new AppSlashCommandBuilder())
-    .setName('invite')
-    .setNameLocalization(Locale.French, 'invitation')
-    .setDescription('Send an invite for the server')
-    .setDescriptionLocalization(Locale.French, 'Envoie une invitation pour le serveur')
+    .setName('command.invite.name')
+    .setDescription('command.invite.description')
     .setDMPermission(false)
     .setCallback(async (client: Client, interaction: CommandInteraction) => {
         const invites: Collection<string, Invite> = await interaction.guild.invites.fetch()
 
         if (invites.size === 0) {
-            await interaction.reply(`Il n'y a pas d'invitation pour ${interaction.guild.name}`)
+            await interaction.reply(
+                translator.getTranslation('command.invite.action.error', interaction.guild.preferredLocale, [interaction.guild.name])
+            )
 
             return
         }
-        await interaction.reply(
-            `Utilise cette invitation pour inviter de nouveaux membres !!!\n${invites.first().url}`
-        )
+        await interaction.reply(translator.getTranslation('command.invite.action.done', interaction.guild.preferredLocale, [invites.first().url]))
     })
 
 export default command
